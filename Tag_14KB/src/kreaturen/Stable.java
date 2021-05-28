e80f50342dec3bd1315d30285a15dac358fa05a4
Kay Böhm <fian-boehm@gmx.net>
2021-05-28 10:10:35 +0200

    just some testing

--- Tag_14KB/src/kreaturen/Stable.java
+++ Tag_14KB/src/kreaturen/Stable.java
@@ -6,6 +6,7 @@ public class Stable {
 	private Creature creature;
 	private Stable nextStable;
 
+
 	public Stable() {
 		super();
 	}
@@ -19,7 +20,7 @@ public class Stable {
 //	public Stable(int anzahlStaelle, Creature... creatures) {
 //
 //	}
-	
+
 	@Override
 	public String toString() {
 		return "Stable [creature=" + creature + ", nextStable=" + nextStable + "]";
@@ -62,8 +63,6 @@ public class Stable {
 //		
 //	}
 
-	
-
 	public void printStable() {
 		if (creature != null) {
 			PrintHelper.printLine();
@@ -84,8 +83,6 @@ public class Stable {
 		}
 	}
 
-	
-
 	public void feedCreatures(int anzahlFutterkuebel) {
 		if (creature != null && anzahlFutterkuebel > 0) {
 			if (creature.isHungry()) {
@@ -104,5 +101,64 @@ public class Stable {
 			System.out.println("All creatures are fed, no food left!");
 		}
 	}
-
+	
+	
+	//testing how to deal with returns in recursive method
+//	public String feedCreatures(int anzahlFutterkuebel) {
+//		
+//		
+//		if (creature != null && anzahlFutterkuebel > 0) {
+//			if (creature.isHungry()) {
+//				creature.feedCreature();
+//				anzahlFutterkuebel--;
+//			}
+//			if (nextStable != null) {
+//				nextStable.feedCreatures(anzahlFutterkuebel);
+//			}
+//		}
+//		
+//		
+//		if ((nextStable == null && anzahlFutterkuebel > 0) || (creature == null && anzahlFutterkuebel > 0)) {
+//			feedFoodLeft();
+//
+//		} else if (creature != null && creature.isHungry() && anzahlFutterkuebel == 0) {
+//			feedNotEnoughFood();
+//
+//		} else if ((creature == null && anzahlFutterkuebel == 0)
+//				|| (!creature.isHungry() && nextStable == null && anzahlFutterkuebel == 0)) {
+//			feedAllFedNoFoodLeft();
+//		}
+//		return "";
+//	}
+//
+//	//methode die den nutzer über das fütterergebnis informiert
+//	public String feedFeedback(int result) {
+//		String text = "";
+//		switch (result) {
+//		case 1:
+//			text = "All creatures are fed, some food is left!";
+//			break;
+//		case 2:
+//			text = "Not enough food, some creatures go hungry!";
+//			break;
+//		case 3:
+//			text = "All creatures are fed, no food left!";
+//			break;
+//		default:
+//			text = "Etwas ist schief gelaufen";
+//			break;
+//		}
+//		return text;
+//	}
+//	
+//	public String feedFoodLeft() {
+//		return "All creatures are fed, some food is left!";
+//	}
+//	public String feedNotEnoughFood() {
+//		return("Not enough food, some creatures go hungry!");
+//	}
+//	public String feedAllFedNoFoodLeft() {
+//		return("All creatures are fed, no food left!");
+//	}
+//
 }
