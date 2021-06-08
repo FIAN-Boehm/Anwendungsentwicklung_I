edb7b012436fc1462e2592302f4696104aa57188
Kay Böhm <fian-boehm@gmx.net>
2021-06-08 11:08:30 +0200

    initial push

---
+++ Tag22_KB/src/aufgabenBinaryCalc/CalcTest.java
@@ -0,0 +1,17 @@
+/*
+ * simple prog that calculates decimal to binary and hex
+ */
+
+package aufgabenBinaryCalc;
+
+public class CalcTest {
+
+	public static void main(String[] args) {
+
+//		CalcGui cg = new CalcGui();
+		//version with exceptionhandling
+		CalcGui2 cg2 = new CalcGui2();
+		
+	}
+
+}
