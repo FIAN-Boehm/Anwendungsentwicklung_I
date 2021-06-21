f90df1fc3c2328f5b718a1c31d3c32de0a1ebca3
Kay Böhm <fian-boehm@gmx.net>
2021-06-21 09:25:42 +0200

    initial push

---
+++ Tag26_KB/src/rechner/ALU.java
@@ -0,0 +1,81 @@
+package rechner;
+
+public class ALU {
+
+	public static double addieren(String eingabe) {
+
+		double ergebnis = 0;
+
+		String tmp = eingabe;
+
+		String a = tmp.substring(0, (tmp.indexOf('+')));
+		String b = tmp.substring(tmp.indexOf('+') + 1);
+
+		ergebnis = Double.parseDouble(a) + Double.parseDouble(b);
+
+		return ergebnis;
+	}
+
+	public static double subtrahieren(String eingabe) {
+
+		double ergebnis = 0;
+
+		String tmp = eingabe;
+
+		String a = tmp.substring(0, (tmp.indexOf('-')));
+		String b = tmp.substring(tmp.indexOf('-') + 1);
+
+		ergebnis = Double.parseDouble(a) - Double.parseDouble(b);
+
+		return ergebnis;
+	}
+
+	public static double multiplizieren(String eingabe) {
+
+		double ergebnis = 0;
+
+		String tmp = eingabe;
+
+		String n = tmp.substring(0, (tmp.indexOf('*')));
+		String m = tmp.substring(tmp.indexOf('*') + 1);
+		ergebnis = Double.parseDouble(n) * Double.parseDouble(m);
+
+		return ergebnis;
+	}
+
+	public static double dividieren(String eingabe) throws ArithmeticException {
+
+		double ergebnis = 0;
+
+		String tmp = eingabe;
+
+		String dividend = tmp.substring(0, (tmp.indexOf('/')));
+		String divisor = tmp.substring(tmp.indexOf('/') + 1);
+		if (divisor.equals("0")) {
+			throw new ArithmeticException();
+		}
+		ergebnis = Double.parseDouble(dividend) / Double.parseDouble(divisor);
+
+		return ergebnis;
+	}
+
+	public static String ausrechnen (String eingabe) {
+		double ergebnis=0;
+		
+		if(eingabe.contains("+")){
+		ergebnis=addieren(eingabe);
+		}
+		else if(eingabe.contains("-")){
+			ergebnis=subtrahieren(eingabe);
+		}
+		else if(eingabe.contains("*")){
+			ergebnis=multiplizieren(eingabe);
+		}
+		else if(eingabe.contains("/")){
+			ergebnis=dividieren(eingabe);
+		}
+		String ergebnisText = ""+ergebnis;
+		return ergebnisText;
+	}
+
+}
