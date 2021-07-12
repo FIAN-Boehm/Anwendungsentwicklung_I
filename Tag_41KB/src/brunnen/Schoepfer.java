8249b6c27bc1cdeea8481b23dcf2337bf4257063
Kay Böhm <fian-boehm@gmx.net>
2021-07-12 08:46:43 +0200

    up

--- Tag_41KB/src/brunnen/Schoepfer.java
+++ Tag_41KB/src/brunnen/Schoepfer.java
@@ -10,33 +10,34 @@ public class Schoepfer extends Thread {
 	@Override
 	public void run() {
 		while (true) {
-			
+
 			if (super.getName().equals("verteiler")) {
 
 				int wassermenge = (int) (Math.random() * 21);
-				if (Ziehbrunnen.getWassertrog() > wassermenge) {
+				if (Ziehbrunnen.getWassertrog() >= wassermenge) {
 					Ziehbrunnen.setWassertrog(Ziehbrunnen.getWassertrog() - wassermenge);
-					System.out.println(wassermenge+ " Liter verteilt");
-					System.out.println(Ziehbrunnen.getWassertrog()+" Liter sind im Brunnen.\n");
-				} else {
+					System.out.println(wassermenge + " Liter verteilt");
+					System.out.println(Ziehbrunnen.getWassertrog() + " Liter sind im Brunnen.\n");
+				} else if (Ziehbrunnen.getWassertrog() < wassermenge) {
 					try {
 						Thread.sleep(5000);
-						System.out.println("Wollte "+wassermenge+" Liter holen, "+Ziehbrunnen.getWassertrog()+" Liter sind nur drin. Ich warte!");
+						System.out.println("Wollte " + wassermenge + " Liter holen, " + Ziehbrunnen.getWassertrog()
+								+ " Liter sind nur drin. Ich warte!");
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}
 
-			} else {
-				if (super.getName().equals("pumpe")) {
-
-					int wassermenge = (int) (Math.random() * 11);
-					Ziehbrunnen.setWassertrog(Ziehbrunnen.getWassertrog() + wassermenge);
-					System.out.println(wassermenge+ " Liter reingepumt");
-					System.out.println(Ziehbrunnen.getWassertrog()+" Liter sind im Brunnen.\n");
-				}
 			}
+			if (super.getName().equals("pumpe")) {
+
+				int wassermenge = (int) (Math.random() * 11);
+				Ziehbrunnen.setWassertrog(Ziehbrunnen.getWassertrog() + wassermenge);
+				System.out.println(wassermenge + " Liter reingepumt");
+				System.out.println(Ziehbrunnen.getWassertrog() + " Liter sind im Brunnen.\n");
+			}
+
 			try {
 				Thread.sleep(3000);
 			} catch (InterruptedException e) {
@@ -44,6 +45,6 @@ public class Schoepfer extends Thread {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
 }
